7fcebbea365560184d74d98822cd663981e19ae1
Sam Selikoff <sam.selikoff@gmail.com>
2021-04-13 15:32:40 -0400

    another

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 export default function Home() {
   return (
-    <div className="flex w-full">
-      <div className="w-64 h-64 bg-blue-500"></div>
-      <div className="w-64 h-64 mx-auto bg-green-500"></div>
-      <div className="w-64 h-64 bg-purple-500"></div>
+    <div className="mx-auto max-w-7xl">
+      <div className="flex w-full">
+        <div className="flex-shrink-0 w-64 h-64 bg-blue-500"></div>
+        <div className="h-64 max-w-md mx-auto">
+          <div className="bg-green-500 ">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            aut vitae voluptatibus ducimus hic tenetur illum, unde reprehenderit
+            velit, doloribus quisquam. Ab corrupti nostrum ut recusandae
+            expedita, dignissimos modi fugiat.
+          </div>
+        </div>
+        <div className="flex-shrink-0 w-64 h-64 bg-purple-500"></div>
+      </div>
     </div>
   );
 }
